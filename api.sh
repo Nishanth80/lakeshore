55435b91285b50b4ebb08e19dda3db5ce0af36bb
Nishanth80 <90023265+Nishanth80@users.noreply.github.com>
2021-09-22 09:32:14 +0530

    Update api.sh

--- api.sh
+++ api.sh
@@ -1,5 +1,5 @@
 #!bin/bash
-LatestbuildNumber=$(curl -X GET "https://api.appcenter.ms/v0.1/apps/nishanth.g-successive.tech/Lakeshoredemo/branches/master/builds" -H  "accept: application/json" -H  "X-API-Token: 89c17c2c10ff3f78ca7da00248eec1da0e80cc54" |jq -r '.[0].buildNumber') 
 #sleep 25m
+LatestbuildNumber=$(curl -X GET "https://api.appcenter.ms/v0.1/apps/nishanth.g-successive.tech/Lakeshoredemo/branches/master/builds" -H  "accept: application/json" -H  "X-API-Token: 89c17c2c10ff3f78ca7da00248eec1da0e80cc54" |jq -r '.[0].buildNumber') 
 echo $LatestbuildNumber
-appcenter build download --app nishanth.g-successive.tech/Lakeshoredemo --type build --id "3"
+appcenter build download --app nishanth.g-successive.tech/Lakeshoredemo --type build --id "$LatestbuildNumber"
