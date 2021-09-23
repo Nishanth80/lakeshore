379f7a49e7712af4a631747b6d8c06726786bd7f
Nishanth80 <90023265+Nishanth80@users.noreply.github.com>
2021-09-23 11:59:07 +0530

    Update browserstack.sh

--- browserstack.sh
+++ browserstack.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-curl -u "abhinavvashishat_2rznLJ:7ezu9G9VfARka2RnAyrV" \
+curl -u "$BrowserStack" \
 -X POST "https://api-cloud.browserstack.com/app-automate/upload" \
 -F "file=/var/lib/jenkins/workspace/Lakeshore-Demo/*.xcarchive"
