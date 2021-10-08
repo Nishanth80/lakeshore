166e075a0d214b6f4f7b0890a8044ff8654c0486
Nishanth80 <90023265+Nishanth80@users.noreply.github.com>
2021-10-08 21:19:10 +0530

    Update app.sh

--- app.sh
+++ app.sh
@@ -4,4 +4,4 @@
     echo $latestTag
     chmod a+x ios/ReactNativeStarter/Info.plist
     sed -i "${20}s/1.0/"$latestTag"-qa/" ios/ReactNativeStarter/Info.plist
-    git push https://github.com/Nishanth80/lakeshore.git 
+    
