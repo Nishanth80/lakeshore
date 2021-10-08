d7d4c659137230d1ec1b6f540d873f17107df78c
Nishanth80 <90023265+Nishanth80@users.noreply.github.com>
2021-10-08 21:16:09 +0530

    Update app.sh

--- app.sh
+++ app.sh
@@ -2,6 +2,6 @@
 
     latestTag=$(git describe --tags $(git rev-list --tags --max-count=1))
     echo $latestTag
-    chmod a+x /var/lib/jenkins/workspace/Release/ios/ReactNativeStarter/Info.plist
-    sed -i "${20}s/1.0/"$latestTag"-qa/" var/lib/jenkins/workspace/Release/ios/ReactNativeStarter/Info.plist
+    chmod a+x ios/ReactNativeStarter/Info.plist
+    sed -i "${20}s/1.0/"$latestTag"-qa/" ios/ReactNativeStarter/Info.plist
     git push https://github.com/Nishanth80/lakeshore.git 
