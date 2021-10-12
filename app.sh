f33b7e42d1231c9cf2c7c03487841d6d551982db
Nishanth80 <90023265+Nishanth80@users.noreply.github.com>
2021-10-12 18:33:04 +0530

    Update app.sh

--- app.sh
+++ app.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
+    
+    sed -i '20s/.\{9\}/&v/' ios/ReactNativeStarter/Info.plist
 
     latestTag=$(git describe --tags $(git rev-list --tags --max-count=1))
     echo $latestTag
     chmod a+x ios/ReactNativeStarter/Info.plist
     oldTag=$(git describe --tags $(git rev-list --tags --max-count=1 --skip=1 --no-walk))
     echo $oldTag
-    sed -i '20s/.\{9\}/&v/' ios/ReactNativeStarter/Info.plist
+    
     sed -i "${20}s/"$oldTag"-qa/"$latestTag"-qa/" ios/ReactNativeStarter/Info.plist
     sed -i '20s/[v]//' ios/ReactNativeStarter/Info.plist   
   
