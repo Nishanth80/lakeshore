aa2d9e04d65cbda5766ec6f777d55586369b700f
Nishanth80 <90023265+Nishanth80@users.noreply.github.com>
2021-10-22 12:01:33 +0530

    Update release.sh

--- release.sh
+++ release.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+sed -i '20s/.\{9\}/&v/' ios/ReactNativeStarter/Info.plist
 latestTag=$(git describe --tags $(git rev-list --tags --max-count=1))
     echo $latestTag
     chmod a+x ios/ReactNativeStarter/Info.plist
     oldTag=$(git describe --tags $(git rev-list --tags --max-count=1 --skip=1 --no-walk))
     echo $oldTag
-    sed -i '20s/.\{9\}/&v/' ios/ReactNativeStarter/Info.plist
+    
 version=$(sed -n '20s/[^0-9]*\(\([0-9]\.\)\{0,4\}[0-9][0-9][^.]\).*/\1/p' ios/ReactNativeStarter/Info.plist)
 echo=$version
 #Initializing two variables
