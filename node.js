d54f87adbcab13f03d1019d0bd7d902d82da433d
Nishanth80 <90023265+Nishanth80@users.noreply.github.com>
2021-10-28 17:13:11 +0530

    Update node.js

--- node.js
+++ node.js
@@ -1,15 +1,14 @@
-#!/usr/bin/env groovy
-userName = chirag_s0hAmI
-accessKey = UKVo1z4FvdVpShc6WdyC
+//userName = chirag_s0hAmI;
+//accessKey = UKVo1z4FvdVpShc6WdyC;
 //browserstackLocal = process.env.BROWSERSTACK_LOCAL
-buildName = LakeshoreProject
+//buildName = LakeshoreProject;
 //browserstackLocalIdentifier = process.env.BROWSERSTACK_LOCAL_IDENTIFIER
-app = build_qa_50_1.ipa
+//app = build_qa_50_1.ipa;
 var capabilities = {
-    "browserstack.user" : userName,
-    "browserstack.key" : accessKey,
-    "app" : app,
-    "build" : buildName,
+    "browserstack.user" : chirag_s0hAmI,
+    "browserstack.key" : UKVo1z4FvdVpShc6WdyC,
+    "app" : build_qa_50_1.ipa,
+    "build" : LakeshoreProject,
     "device" : "iPhone 11",
     //"browserstack.local" : browserstackLocal,
     //"browserstack.localIdentifier" : browserstackLocalIdentifier
